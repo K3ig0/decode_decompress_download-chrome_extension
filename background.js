7ce931c139b4a8c7c0ce265b1a548186c8b8e8fa
Manu <manuel.sanchez.naveira@gmail.com>
2024-05-02 00:15:15 +0200

    1.0.1: A notification is displayed when the extension has no access to file content

--- background.js
+++ background.js
@@ -1,14 +1,27 @@
 chrome.action.onClicked.addListener(async function(tab){
-  await chrome.scripting.executeScript({
-    target: { tabId: tab.id, allFrames: true },
-    files: ["lib/snappyjs.min.js"],
-  });
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: decodeDecompressAndDownload
+  chrome.extension.isAllowedFileSchemeAccess(async function(isAllowedAccess) {
+      if (!isAllowedAccess) {
+        console.warn("You must allow access to file URLS");
+        chrome.notifications.create("You must allow access to file URLS", {
+          title: "No access to the content file",
+          message: "You must allow access to file URLS",
+          iconUrl: "/logo/logo.png",
+          imageUrl: "/thumbnail/allow_access_to_file_urls.jpg",
+          type: "image",
+          requireInteraction: true
+        });
+        return;
+      }
+      await chrome.scripting.executeScript({
+        target: { tabId: tab.id, allFrames: true },
+        files: ["lib/snappyjs.min.js"],
+      });
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: decodeDecompressAndDownload
+      });
   });
 });
-
 
 function decodeDecompressAndDownload(tabId) {
   const binaryStr = atob(document.body.innerText);
@@ -19,9 +32,9 @@ function decodeDecompressAndDownload(tabId) {
   }
   const uncompresed = window.SnappyJS.uncompress(bytes);
   const str = new TextDecoder("utf-8").decode(uncompresed);
-  let hiddenElement = document.createElement('a');
-  hiddenElement.href = 'data:image/svg+xml;base64,' + btoa(str);
-  hiddenElement.target = '_blank';
-  hiddenElement.download = window.location.href.split("/").pop() + '.svg';
+  let hiddenElement = document.createElement("a");
+  hiddenElement.href = "data:image/svg+xml;base64," + btoa(str);
+  hiddenElement.target = "_blank";
+  hiddenElement.download = window.location.href.split("/").pop() + ".svg";
   hiddenElement.click();
 };
